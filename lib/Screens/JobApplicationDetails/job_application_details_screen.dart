635550e4763b5dbbd6138b9ec6f6fb60df98ef67
Amit Patil <amitgpatil215@gmail.com>
2021-04-24 23:36:52 +0530

    info card in more details creen

--- lib/Screens/JobApplicationDetails/job_application_details_screen.dart
+++ lib/Screens/JobApplicationDetails/job_application_details_screen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:intertrack/Utils/utils.dart';
 import 'package:timeline_tile/timeline_tile.dart';
 
@@ -7,56 +9,142 @@ class JobApplicationDetailsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(),
-        body: Column(
-          children: [
-            ListTile(
-              title: Text('Uber Ink'),
-              subtitle: Text('Product designer'),
-            ),
-            for (int i = 0; i < 3; i++)
-              TimelineTile(
-                alignment: TimelineAlign.manual,
-                lineXY: 0.25,
-                startChild: SizedBox(
-                  height: Responsive().height * 0.085,
-                  child: Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Text('Aug'),
-                        Text(
-                          '20',
-                          style: TextStyle(
-                            fontSize: Responsive().extraLargeW,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ],
-                    ),
+        backgroundColor: scaffoldBGColor,
+        appBar: AppBar(
+          actions: [
+            IconButton(
+              icon: Icon(
+                Icons.edit,
+                size: Responsive().extraLargeW,
+              ),
+              onPressed: () {},
+            )
+          ],
+          iconTheme: IconThemeData(
+            color: Colors.black,
+          ),
+          backgroundColor: scaffoldBGColor,
+          elevation: 0,
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              ListTile(
+                title: Text(
+                  'Uber Ink',
+                  style: TextStyle(
+                    fontSize: Responsive().extraLargeW,
+                    fontWeight: FontWeight.w600,
                   ),
                 ),
-                endChild: SizedBox(
-                  height: Responsive().height * 0.085,
-                  child: Center(
-                    child: Row(
-                      children: [
-                        SizedBox(
-                          width: Responsive().smallW,
-                        ),
-                        SizedBox(
-                          width: Responsive().width * 0.65,
-                          child: Text(
-                            'Application Submitted',
+                subtitle: Text('Product designer'),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: Responsive().smallW,
+                ),
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(
+                      Responsive().smallW,
+                    ),
+                  ),
+                  child: Column(
+                    children: [
+                      MoreDetailsInfoCardListTile(
+                        icondata: CupertinoIcons.calendar,
+                        title: 'Application submitted on',
+                        subTitle: 'Tuesday, 27 April 2021',
+                      ),
+                      MoreDetailsInfoCardListTile(
+                        icondata: FontAwesomeIcons.checkCircle,
+                        title: 'Application Status',
+                        subTitle: 'Researching',
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              ListView.builder(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                itemCount: 5,
+                itemBuilder: (context, index) => TimelineTile(
+                  alignment: TimelineAlign.manual,
+                  lineXY: 0.25,
+                  //isFirst: index == 0 ? true : false,
+                  isLast: index == 4 ? true : false,
+                  startChild: SizedBox(
+                    height: Responsive().height * 0.085,
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text('Aug'),
+                          Text(
+                            '20',
+                            style: TextStyle(
+                              fontSize: Responsive().extraLargeW,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
+                    ),
+                  ),
+                  endChild: SizedBox(
+                    height: Responsive().height * 0.085,
+                    child: Center(
+                      child: Row(
+                        children: [
+                          SizedBox(
+                            width: Responsive().smallW,
+                          ),
+                          SizedBox(
+                            width: Responsive().width * 0.65,
+                            child: Text(
+                              'Application Submitted',
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
               ),
-          ],
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MoreDetailsInfoCardListTile extends StatelessWidget {
+  const MoreDetailsInfoCardListTile({
+    this.title = '',
+    this.subTitle = '',
+    this.icondata,
+    Key? key,
+  }) : super(key: key);
+
+  final String title;
+  final String subTitle;
+  final IconData? icondata;
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      dense: true,
+      minLeadingWidth: Responsive().smallW,
+      leading: FaIcon(icondata),
+      title: Text(title),
+      subtitle: Text(
+        subTitle,
+        style: TextStyle(
+          fontSize: Responsive().largeW,
+          color: Colors.black,
+          fontWeight: FontWeight.w400,
         ),
       ),
     );
