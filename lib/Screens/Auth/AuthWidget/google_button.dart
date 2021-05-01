e22aa5dbc8fbd2795418892ceb96d695ae72e32e
Amit Patil <amitgpatil215@gmail.com>
2021-05-02 02:54:39 +0530

    refactoring google button

---
+++ lib/Screens/Auth/AuthWidget/google_button.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:intertrack/Utils/utils.dart';
+
+class GoogleButton extends StatelessWidget {
+  const GoogleButton({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: Responsive().mediumW),
+      child: Center(
+        child: ElevatedButton(
+          onPressed: () {},
+          child: Row(
+            children: [
+              CircleAvatar(
+                backgroundColor: Colors.white,
+                radius: Responsive().largeW,
+                child: FaIcon(
+                  FontAwesomeIcons.google,
+                  size: Responsive().largeW,
+                ),
+              ),
+              Expanded(
+                child: Center(
+                  child: Text('Sign in with google'),
+                ),
+              )
+            ],
+          ),
+          style: ButtonStyle(
+            backgroundColor:
+                MaterialStateProperty.resolveWith((states) => navy),
+            padding: MaterialStateProperty.resolveWith(
+              (states) => EdgeInsets.symmetric(
+                vertical: Responsive().mediumW,
+                horizontal: Responsive().extraLargeW,
+              ),
+            ),
+            shape: MaterialStateProperty.resolveWith(
+              (states) => RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(Responsive().width * 0.5),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/Screens/Auth/login_screen.dart
+++ lib/Screens/Auth/login_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:intertrack/Screens/Auth/AuthWidget/AuthWidget.dart';
 import 'package:intertrack/Utils/utils.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -17,47 +17,7 @@ class LoginScreen extends StatelessWidget {
             ),
           ),
           Spacer(),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: Responsive().mediumW),
-            child: Center(
-              child: ElevatedButton(
-                onPressed: () {},
-                child: Row(
-                  children: [
-                    CircleAvatar(
-                      backgroundColor: Colors.white,
-                      radius: Responsive().largeW,
-                      child: FaIcon(
-                        FontAwesomeIcons.google,
-                        size: Responsive().largeW,
-                      ),
-                    ),
-                    Expanded(
-                      child: Center(
-                        child: Text('Sign in with google'),
-                      ),
-                    )
-                  ],
-                ),
-                style: ButtonStyle(
-                  backgroundColor:
-                      MaterialStateProperty.resolveWith((states) => navy),
-                  padding: MaterialStateProperty.resolveWith(
-                    (states) => EdgeInsets.symmetric(
-                      vertical: Responsive().mediumW,
-                      horizontal: Responsive().extraLargeW,
-                    ),
-                  ),
-                  shape: MaterialStateProperty.resolveWith(
-                    (states) => RoundedRectangleBorder(
-                      borderRadius:
-                          BorderRadius.circular(Responsive().width * 0.5),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ),
+          GoogleButton(),
           SizedBox(
             height: Responsive().smallH,
           ),
