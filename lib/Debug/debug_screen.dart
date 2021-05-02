1a376ca9107c91411c997a4e06ce99614c789a6a
Amit Patil <amitgpatil215@gmail.com>
2021-05-02 15:54:59 +0530

    adding log out button in debug screen

--- lib/Debug/debug_screen.dart
+++ lib/Debug/debug_screen.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:intertrack/Controller/auth_controller.dart';
 
 class DebugScreen extends StatelessWidget {
+  final AuthController? _authController = Get.put(AuthController());
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: ElevatedButton(
-          onPressed: () {},
+          onPressed: () async {
+            await _authController?.logout();
+          },
           child: Text('Log Out'),
         ),
       ),
